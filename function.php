325d47e112b347349771009990c547c2c77b4e17
theoregons <baktiarmahardika08@gmail.com>
2024-05-16 11:37:51 +0700

    Halaman Produk

--- function.php
+++ function.php
@@ -28,5 +28,20 @@
         
         mysqli_query($koneksi, $query);
     }
+    
+    if(isset($_POST['tambahproduk'])){
+        $nama_produk = $_POST['nama_produk'];
+        $deskripsi = $_POST['deskripsi'];
+        $harga = $_POST['harga'];
+        $stock = $_POST['stock'];
 
+        $insert_produk = mysqli_query($koneksi, "INSERT INTO produk (nama_produk, deskripsi, harga, stock) VALUES ('$nama_produk', '$deskripsi', '$harga', '$stock')");
+
+        if($insert_produk){
+            header('location:stock.php');
+        }else{
+            echo '<script type="text/javascript"> alert("Gagal!"); </script>';
+            header('location:stock.php');
+        }
+    }
 ?>
